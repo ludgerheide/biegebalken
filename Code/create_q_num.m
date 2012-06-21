418f421a0d9bb1e7916235bdbf060b97a7a57cce
Ludger Heide <ludger.heide@gmail.com>
2012-06-21 19:28:19 +0200

    Lösung für den statischen Fall (läuft aber Ergebnis nicht ganz korrekt)

---
+++ Code/create_q_num.m
@@ -0,0 +1,72 @@
+function q_ = create_q_num(q, n, L, precision)
+
+% h ist die Länge eines Intervalls
+h = L/(n-1);
+
+% x ist ein Vektor von 0 bis L, der jeweils zwischen den Stützstellen
+% precision Werte enthält
+x = linspace(0,L,(n-1)*(1/precision));
+
+%X ist ein Vektor, der die x-Koordinaten aller Knoten enthält
+X = linspace(0,L,n);
+ 
+
+% Erster und zweiter von Hand, um outofbounds zu vermeiden
+q_(1)=quad(@(x)(q(x).*phi2i_1(x, X, 1, h, n)), 0, h, precision);
+q_(2)=quad(@(x)(q(x).*phi2i(x, X, 1, h, n)), 0, h, precision);
+
+% Vorletzter und Letzte von Hand, um outofbounds zu vermeiden
+q_(2*n-1)=quad(@(x)(q(x).*phi2i_1(x, X, n-1, h, n)), L-h, L, precision);
+q_(2*n)=quad(@(x)(q(x).*phi2i(x, X, n-1, h, n)), L-h, L, precision);
+
+% Dazwischenliegende qs in einer Schleife
+for j=3:2*n-2
+    if mod(j,2)==0
+        %i gerade
+        i=(j/2);
+        q_(j)=quad(@(x)(q(x).*phi2i(x, X, i, h, n)), X(i-1), X(i+1), precision);
+    else
+        %i ungerade
+        i=(j+1)/2;
+        q_(j)=quad(@(x)(q(x).*phi2i_1(x, X, i, h, n)), X(i-1), X(i+1), precision);
+    end
+end
+q_ = q_';
+end
+function p2_1 = phi2i_1(x,X,i,h,n)
+% Funktionsdeklarationen von phiQuer
+phiQuer1 = @(z) (1 - z.^2 + 2.*z.^3);
+phiQuer2 = @(z) (z.*(z-1).^2);
+phiQuer3 = @(z) (3.*z.^2-2.*z.^3);
+phiQuer4 = @(z) (z.^2.*(z-1));
+    if i==1
+        %Phi(1)
+        p2_1 = (phiQuer1(x)) .* and(X(1)*ones(size(x)) <= x , x <= X(2)*ones(size(x)));
+    elseif i==n
+        %Phi(2n-1)
+        p2_1 = (phiQuer3((x - X(n-1)) / h) .* and ( X(n-1)*ones(size(x)) <= x, x <= X(n)*ones(size(x))));
+    else
+        %Phi(2i-1)
+        p2_1 = (phiQuer3((x - X(i-1)) / h) .* and ( X(i-1)*ones(size(x)) <= x, x <= X(i)*ones(size(x))) + ...
+                phiQuer1((x - X(i)) / h) .* and ( X(i)*ones(size(x)) <= x, x <= X(i+1)*ones(size(x))));
+    end
+end
+
+function p2 = phi2i(x,X,i,h,n)
+% Funktionsdeklarationen von phiQuer
+phiQuer1 = @(z) (1 - z.^2 + 2.*z.^3);
+phiQuer2 = @(z) (z.*(z-1).^2);
+phiQuer3 = @(z) (3.*z.^2-2.*z.^3);
+phiQuer4 = @(z) (z.^2.*(z-1));
+    if i==1
+        %Phi(2)
+        p2 = (h .* phiQuer2(x)) .* and(X(1)*ones(size(x)) <= x, x <= X(2)*ones(size(x)));
+    elseif i==n
+        %Phi(2n)
+        p2 = (h .* phiQuer4((x - X(n-1)) / h) .* and ( X(n-1)*ones(size(x)) <= x, x <= X(n)*ones(size(x))));
+    else
+        %Phi(2i)
+        p2 = (h .* phiQuer4((x - X(i-1)) / h) .* and ( X(i-1)*ones(size(x)) <= x, x <= X(i)*ones(size(x))) + ...
+              h .* phiQuer2((x - X(i)) / h) .* and ( X(i)*ones(size(x)) <= x, x <= X(i+1)*ones(size(x))));
+    end
+end
---
+++ Code/solve_static.m
@@ -0,0 +1,32 @@
+function [u, L1, L2] = solve_static(S, q_, lager, a, b)
+% Berechnet den Vektor u, sowie die Lagerreaktionen L1 und L2
+% L1 und L2 können abhängig von der Lagerung Momente oder Kräfte sein
+% Eigabewerte:  S ist die Matrix S, numerisch oder analytisch erstellt
+%               a und b sind die Randbedingungen
+%               q_ ist der Vektor mit den Integralen für q*phi
+%               lager steht für die Art der Lagerung
+%               Mögliche Werte: 1 Feste Einspannung     Kein Lager
+
+ed1=zeros(size(S),1);
+ed2=zeros(size(S),1);
+
+switch lager
+    case 1
+        %Wir haben ein einseitiges Festlager
+        ed1(1)=1
+        ed2(2)=1
+    case 2
+        sprintf('blubb');
+end
+   
+CSST=[S ed1 ed2; ed1' 0 0; ed2' 0 0];
+
+fab= [q_; a; b];
+
+loesung=CSST\fab;
+
+u=loesung(1:size(loesung)-2);
+%L1=loesung(size(loesung)-1);
+%L2=loesung(size(loesung));
+
+end
