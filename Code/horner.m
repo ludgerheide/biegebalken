43af9a1f98864179f12a83dca96559027f5d363b
Tillmann Fiehn <f.i.e.h.n.tillmann@googlemail.com>
2012-06-28 15:49:08 +0200

    Erster Commit von Tillmann

--- Code/horner.m
+++ Code/horner.m
@@ -1,6 +1,6 @@
 function [ f ] = horner( A, X )
 %HORNER(A,X) Wertet einen Polynom in Koeffizientendarstellung an den
-%Stellen X aus
+%Stellen X mit Hilfe des Horner-Schemas aus
   g = zeros(size(X));
   for a=A
     f = a * ones(size(X)) + g;
