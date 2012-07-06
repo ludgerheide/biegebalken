11bcb74eb8c3cf21840a2a699d3b69c9d909f753
Ludger Heide <ludger.heide@gmail.com>
2012-07-06 16:30:49 +0200

    Zeitlupe/Zeitraffer ermöglicht

--- Code/dynamictest.m
+++ Code/dynamictest.m
@@ -9,15 +9,17 @@ q=@(x)0;
 
 mu=@(x)1;
 
-fps = 30;
-secs = .25;
+%Zeitfaktoren HIER
+fps = 30; %Bilder pro Sekunde im Video
+secs = 10; %Länge des Videos
+timescale=100; %Skalierung, <1 für zeitraffer, >1 für Zeitlupe
 
-ht=1/fps;
+ht=1/(timescale*fps);
 N=secs*fps;
 
 L=2;
-n=3;
-precision=.00001;
+n=15;
+precision=.0001;
 
 tic;
 S=create_S_num(E,I,L,n,precision);
@@ -28,14 +30,14 @@ a=0;
 b=0;
 
 [u, L1, L2] = solve_static(S, q_, lager, a, b);
-u_=[u; L1; L2];
+u_=[zeros((2*n)-6,1); 0; 1; 1; 0; 0; 0; L1; L2];
 time=toc;
 fprintf('Statischer Fall in %2.1f Sekunden geloest.\n', time);
 
 tic;
 %q fuer den dynamischen Fall
-q = @(x,t) -1*sin(10*t);
-q_= @(t) create_q_num(@(x)q(x,t),n,L,precision);
+q = @(x,t)0;
+q_= @(t) create_q_num(q,n,L,precision);
 M=create_M_num(mu,L,n,precision);
 time=toc;
 fprintf('Dynamischer Fall in %2.1f Sekunden vorbereitet.\n', time);
@@ -46,7 +48,7 @@ time=toc;
 fprintf('Dynamischer Fall in %2.1f Sekunden geloest.\n', time);
 
 tic;
-biegelinienfilm(U, L, fps, 'Beidseitige Loslagerung, Anregung unter Resonanzfrquenz', q);
+biegelinienfilm(U, L, fps, 'Welle im unbelasteten Walken, 100x Zeitlupe', q);
 time=toc;
 fprintf('Dynamischer Fall in %2.1f Sekunden gefilmt.\n', time);
 
